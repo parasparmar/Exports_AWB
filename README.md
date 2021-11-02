e5392cec7f4afb262f6483c7dc8cb3be427de7e2
parasparmar <parasparmar@gmail.com>
2021-11-02 18:23:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-iframe-src-4pt8hz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-iframe-src-4pt8hz)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Paras';
+  url: string = 'AWB.html';
+  urlSafe: SafeResourceUrl;
+
+  constructor(public sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
+}
